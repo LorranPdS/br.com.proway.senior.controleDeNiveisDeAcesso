d8503830089b047c402854ecaa951a30fda033d4
Janaina Mai <janainamai@hotmail.com>
2021-05-24 08:58:34 -0300

    Criada classe PermissaoDTO, PermissaoControllerApi, documentação revisada.

--- src/main/java/model/dto/PermissaoDTO.java
+++ src/main/java/model/dto/PermissaoDTO.java
@@ -1,5 +1,23 @@
 package model.dto;
 
+import model.entidades.Permissao;
+
 public class PermissaoDTO {
+
+	private Integer idPermissao;
+	private String nomePermissao;
+
+	public PermissaoDTO(Permissao permissao) {
+		this.idPermissao = permissao.getIdPermissao();
+		this.nomePermissao = permissao.getNomePermissao();
+	}
+
+	public Integer getIdPermissao() {
+		return idPermissao;
+	}
+
+	public String getNomePermissao() {
+		return nomePermissao;
+	}
 
 }
