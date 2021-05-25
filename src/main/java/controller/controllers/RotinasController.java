cc81f9b173eceab20690d0138d36d72fdf3a4c95
marcelo schaefer <marceloschaeferfilho@gmail.com>
2021-05-25 13:21:24 -0300

    configuraçoes spring

--- src/main/java/controller/controllers/RotinasController.java
+++ src/main/java/controller/controllers/RotinasController.java
@@ -13,6 +13,7 @@ public class RotinasController {
 	PerfilDeUsuarioController perfilDeUsuarioController = new PerfilDeUsuarioController();
 
 	/**
+	 * 
 	 * Desativa todas as permissoes expiradas.
 	 * 
 	 * Lista todos os registros da tabela {@link PerfilDeUsuario} e seta 'ativo'
