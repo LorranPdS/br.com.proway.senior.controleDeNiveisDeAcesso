2535066bcace55087bcb92685b6ea31e623458d3
Danilira <dev.danilira@gmail.com>
2021-06-10 16:07:39 -0300

    login atualizado

---
+++ src/main/java/angular/src/app/services/login.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { LoginComponent} from '../login/login.component';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+
+  private loginUrl = 'http://localhost:4200/login';
+
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+
+  constructor(private http: HttpClient) { }
+
+}
