3c36f2f1fa29ceef80b19d91c9c1139707112baa
SENIOR <SENIOR@proway.treina>
2021-06-10 10:02:44 -0300

    primeiros passos para o service'

---
+++ src/main/java/angular/src/app/services/permissao.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { Permissao } from 'app/entidades/permissao';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PermissaoService {
+
+  private permissaoUrl = 'localhost:8080/permissao/listar';
+
+  constructor(private http: HttpClient) { }
+
+  getPermissao(): Observable<Permissao[]> {
+    return this.http.get<Permissao[]>(this.permissaoUrl)
+  }
+}
